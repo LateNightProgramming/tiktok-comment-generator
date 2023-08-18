1b734ea5e5c5440376e5cf1ec63eac0170e6df32
LateNightProgramming <124677881+LateNightProgramming@users.noreply.github.com>
2023-08-18 12:35:33 +1000

    Update generator.py
    improved source code

--- generator.py
+++ generator.py
@@ -1,30 +1,35 @@
-import random as r
+from random import randint, choice, getrandbits
 
-main = ''
-p1 = ['bro is','his fade be','ayo','he be lookin']
-p2 = ['vovacious','zesty','sus','a silly billy','carmunkulous']
-p2_1 = ['vovacious','zesty','sus','carmunkulous']
-p3 = ['🔥','💀','🥶','🥵','❗','😂','😬']
-
-p1text = r.choice(p1)
-if p1text == 'ayo':
-    main += p1text
-    main += ' '
-    p1.remove('ayo')
-    p1text = r.choice(p1)
-main += p1text
-main += ' '
-if p1text == 'his fade be' or p1text == 'he be lookin':
-    p2text = r.choice(p2_1)
-    main += p2text
-else:
-    p2text = r.choice(p2)
-    main += p2text
-p4 = r.choice([True, False])
-if p4 == True:
-    main += r.choice([' in ohio',', only in ohio',' in oklahoma',' only in oklahoma'])
-times = r.randint(2,15)
-for x in range(times):
-    p3text = r.choice(p3)
-    main += p3text
-print(main)
+def main():
+    string = ""
+    p1 = ['bro is','his fade be','he be lookin']
+    p2 = [' vovacious',' zesty',' sus',' a silly billy',' carmunkulous']
+    p2_1 = [' vovacious',' zesty',' sus',' carmunkulous']
+    p3 = ['🔥','💀','🥶','🥵','❗','😂','😬']
+    selection = choice(p1)
+    if bool(getrandbits(1)):
+        string+="ayo "
+    string+=selection
+    if selection in ("his fade be","he be lookin"):
+        string+=choice(p2_1)
+    else:
+        string+=choice(p2)
+    selection = bool(getrandbits(1))
+    if selection:
+        string+=choice([" in ohio", " only in ohio", " in oklahoma", " only in oklahoma"])
+    for i in range(randint(2,15)):
+        string+=choice(p3)
+    print("comment:",string)
+    while True:
+        inp = input("do you wish to restart: ").lower()
+        if inp == "yes":
+            print("restarting")
+            main()
+            break
+        elif inp == "no":
+            print("quitting")
+            exit()
+        else:
+            print("invalid response")
+            
+main()
